d356bbc96aeb68239d54e7f39941a85e0bb98183
Andrey <rybalko102@gmail.com>
2026-03-17 18:25:32 +0300

    add Project

---
+++ Andrey_Rybalko/number_to_text_translation.py
@@ -0,0 +1,30 @@
+def check_enter_value():
+    while True:
+        try:
+            number = int(input("Введите число от 1 до 5: "))
+
+            if number > 5 or number < 1:
+                print("Ошибка! Число должно быть в диапазона от 1 до 5!")
+
+                continue
+
+            return number
+
+        except ValueError:
+            print("Ошибка! Необходимо ввести целое число!")
+
+def get_number_text(number):
+    match number:
+        case 1:
+            return "One"
+        case 2:
+            return "Two"
+        case 3:
+            return "Three"
+        case 4:
+            return "Four"
+        case 5:
+            return "Five"
+    return "Ошибка при попытке преобразовать число в текст!"
+
+print(get_number_text(check_enter_value()))
